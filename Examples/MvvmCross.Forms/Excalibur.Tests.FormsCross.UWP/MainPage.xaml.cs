d2371b817209c40a2314f820890a2d1d13c80063
Xciles <xciles@gmail.com>
2017-05-30 22:27:29 +0200

    Adding Forms application Will add MvvmCross to it!

---
+++ Examples/MvvmCross.Forms/Excalibur.Tests.FormsCross.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Excalibur.Tests.FormsCross.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new Excalibur.Tests.FormsCross.App());
+        }
+    }
+}
---
+++ Examples/MvvmCross.Forms/Excalibur.Tests.FormsCross/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using Excalibur.Tests.FormsCross.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Excalibur.Tests.FormsCross.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
