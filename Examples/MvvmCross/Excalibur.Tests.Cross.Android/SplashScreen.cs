bd579fb025468aefaff26658ac9305087f3aac6a
Xciles <xciles@gmail.com>
2017-06-02 23:54:52 +0200

    Adding styles

--- Examples/MvvmCross/Excalibur.Tests.Cross.Android/SplashScreen.cs
+++ Examples/MvvmCross/Excalibur.Tests.Cross.Android/SplashScreen.cs
@@ -4,17 +4,15 @@ using MvvmCross.Droid.Views;
 
 namespace Excalibur.Tests.Cross.Droid
 {
-    [Activity(
-        Label = "Excalibur.Tests.Cross.Droid"
-        , MainLauncher = true
-        , Icon = "@drawable/icon"
-        , Theme = "@style/Theme.Splash"
-        , NoHistory = true
-        , ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "Excalibur.Droid",
+                MainLauncher = true,
+                Icon = "@drawable/icon",
+                Theme = "@style/Theme.Splash",
+                NoHistory = true,
+                ScreenOrientation = ScreenOrientation.Portrait)]
     public class SplashScreen : MvxSplashScreenActivity
     {
-        public SplashScreen()
-            : base(Resource.Layout.SplashScreen)
+        public SplashScreen() : base(Resource.Layout.SplashScreen)
         {
         }
     }
