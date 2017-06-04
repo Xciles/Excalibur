2ce6d3c0dca79176140468fd292c54e73e8eee8f
Xciles <xciles@gmail.com>
2017-06-04 18:57:03 +0200

    Adding base items to iOS

---
+++ Examples/MvvmCross/Excalibur.Tests.Cross.iOS/Bootstrap/ResourceLoaderPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Excalibur.Tests.Cross.iOS.Bootstrap
+{
+    public class ResourceLoaderPluginBootstrap
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.ResourceLoader.PluginLoader, MvvmCross.Plugins.ResourceLoader.iOS.Plugin>
+    {
+    }
+}
