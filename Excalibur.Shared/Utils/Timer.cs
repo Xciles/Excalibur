559071f7a840c6dbcdd014c8076743de1a6e75ab
Xciles <xciles@gmail.com>
2017-06-10 23:38:02 +0200

    Adding timer comments

--- Excalibur.Shared/Utils/Timer.cs
+++ Excalibur.Shared/Utils/Timer.cs
@@ -4,8 +4,19 @@ using System.Threading.Tasks;
 
 namespace Excalibur.Shared.Utils
 {
+    /// <summary>
+    /// PCL111 does not have a Timer implementation. This provides one.
+    /// </summary>
     public sealed class Timer : CancellationTokenSource
     {
+        /// <summary>
+        /// Initializes a new timer using ints to measure time intervals. 
+        /// </summary>
+        /// <param name="callback">A delegate that represents a method that will be executed</param>
+        /// <param name="state">Object (or null) containing information to be used in the callback</param>
+        /// <param name="millisecondsDueTime">The amount of time to wait before the callback is initially invoked.</param>
+        /// <param name="millisecondsPeriod">The interval time between invocations</param>
+        /// <param name="waitForCallbackBeforeNextPeriod">Used to set if the period interval should wait until previous execution finises</param>
         public Timer(Action<object> callback, object state, int millisecondsDueTime, int millisecondsPeriod, bool waitForCallbackBeforeNextPeriod = false)
         {
             Task.Delay(millisecondsDueTime, Token).ContinueWith(async (t, s) =>
@@ -27,6 +38,10 @@ namespace Excalibur.Shared.Utils
             }, Tuple.Create(callback, state), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion, TaskScheduler.Default);
         }
 
+        /// <summary>
+        /// Releases all resources used by the current instance of Timer and will Cancel the token source.
+        /// </summary>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
