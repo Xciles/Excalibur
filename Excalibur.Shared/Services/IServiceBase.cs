5bd5e876e01f2091dbf0dafb8787a518d0bacca4
Xciles <xciles@gmail.com>
2017-06-11 23:23:59 +0200

    Commenting service

--- Excalibur.Shared/Services/IServiceBase.cs
+++ Excalibur.Shared/Services/IServiceBase.cs
@@ -2,8 +2,16 @@
 
 namespace Excalibur.Shared.Services
 {
+    /// <summary>
+    /// Interface for a service class that will manage communication with internal and external endpoints
+    /// </summary>
+    /// <typeparam name="T">The type of the object that is used for communication</typeparam>
     public interface IServiceBase<T>
     {
+        /// <summary>
+        /// Base method for syncing data.
+        /// </summary>
+        /// <returns>An await able Task with the resulting objects as result</returns>
         Task<T> SyncDataAsync();
     }
 }
