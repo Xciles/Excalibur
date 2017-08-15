6548d704ff9db77aa0c6cff0f8898f82080c6b17
Xciles <xciles@gmail.com>
2017-08-15 22:04:31 +0200

    Adding few timer tests

---
+++ Tests/Excalibur.Shared.Tests/Utils/TimerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Excalibur.Shared.Tests.Utils
+{
+    [TestClass]
+    public class TimerTests
+    {
+        [TestMethod]
+        public void SingleTriggerTest()
+        {
+            var mre = new ManualResetEvent(false);
+
+            var wasHit = false;
+            var timer = new Excalibur.Shared.Utils.Timer(state =>
+            {
+                wasHit = true;
+                mre.Set();
+            }, null, 1, -1);
+
+            Assert.IsNotNull(timer);
+            mre.WaitOne();
+
+            Assert.IsTrue(wasHit);
+        }
+
+        [TestMethod]
+        public void SingleImediateTriggerTest()
+        {
+            var mre = new ManualResetEvent(false);
+
+            var wasHit = false;
+            var timer = new Excalibur.Shared.Utils.Timer(state =>
+            {
+                wasHit = true;
+                mre.Set();
+            }, null, -1, -1);
+
+            Assert.IsNotNull(timer);
+            mre.WaitOne();
+
+            Assert.IsTrue(wasHit);
+        }
+    }
+}
