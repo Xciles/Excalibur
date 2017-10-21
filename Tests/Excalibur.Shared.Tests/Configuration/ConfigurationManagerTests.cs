f1736885c6e7c6c29c79e65506beb55fa1fd3935
Xciles <xciles@gmail.com>
2017-10-21 23:47:06 +0200

    Adding some tests that weren't commited yet

---
+++ Tests/Excalibur.Shared.Tests/Configuration/ConfigurationManagerTests.cs
@@ -0,0 +1,158 @@
+﻿using System.Threading.Tasks;
+using Excalibur.Shared.Configuration;
+using Excalibur.Shared.Storage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using XLabs.Ioc;
+
+namespace Excalibur.Shared.Tests.Configuration
+{
+    [TestClass]
+    public class ConfigurationManagerTests
+    {
+        public class ConfigTest
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
+        private Mock<IStorageService> _mockedStorageService;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var container = new SimpleContainer();
+
+            _mockedStorageService = new Mock<IStorageService>();
+            container.Register<IStorageService>(_mockedStorageService.Object);
+
+            Resolver.SetResolver(container.GetResolver());
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Resolver.ResetResolver();
+        }
+
+        [TestMethod]
+        public void CreationTest()
+        {
+            var manager = new ConfigurationManager();
+            Assert.IsNotNull(manager);
+        }
+
+        [TestMethod]
+        public async Task LoadConfigurationAsync()
+        {
+            var config = new ConfigTest()
+            {
+                Description = "Test",
+                Name = "Test"
+            };
+
+            _mockedStorageService.Setup(x => x.ReadAsTextAsync(It.IsAny<string>(), It.IsAny<string>()))
+                                .Returns(() => Task.FromResult(JsonConvert.SerializeObject(config)));
+
+            var manager = new ConfigurationManager();
+            var loadedConfig = await manager.LoadAsync<ConfigTest>();
+
+            Assert.IsNotNull(loadedConfig);
+            Assert.AreEqual(config.Description, loadedConfig.Description);
+            Assert.AreEqual(config.Name, loadedConfig.Name);
+
+            _mockedStorageService.Verify(x => x.ReadAsTextAsync(It.IsAny<string>(), It.IsAny<string>()));
+        }
+
+        [TestMethod]
+        public async Task LoadNullConfigurationAsync()
+        {
+            var manager = new ConfigurationManager();
+            var loadedConfig = await manager.LoadAsync<ConfigTest>();
+
+            Assert.IsNotNull(loadedConfig);
+            Assert.IsTrue(string.IsNullOrEmpty(loadedConfig.Description));
+            Assert.IsTrue(string.IsNullOrEmpty(loadedConfig.Name));
+
+            _mockedStorageService.Verify(x => x.ReadAsTextAsync(It.IsAny<string>(), It.IsAny<string>()));
+        }
+
+        [TestMethod]
+        [ExpectedException((typeof(JsonReaderException)))]
+        public async Task LoadIncorrectJsonConfigurationAsync()
+        {
+            _mockedStorageService.Setup(x => x.ReadAsTextAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(() => Task.FromResult("{[[]}"));
+
+            var manager = new ConfigurationManager();
+            var loadedConfig = await manager.LoadAsync<ConfigTest>();
+        }
+
+        [TestMethod]
+        public async Task LoadEmptyJsonConfigurationAsync()
+        {
+            _mockedStorageService.Setup(x => x.ReadAsTextAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(() => Task.FromResult("{}"));
+
+            var manager = new ConfigurationManager();
+            var loadedConfig = await manager.LoadAsync<ConfigTest>();
+
+            Assert.IsNotNull(loadedConfig);
+            Assert.IsTrue(string.IsNullOrEmpty(loadedConfig.Description));
+            Assert.IsTrue(string.IsNullOrEmpty(loadedConfig.Name));
+
+            _mockedStorageService.Verify(x => x.ReadAsTextAsync(It.IsAny<string>(), It.IsAny<string>()));
+        }
+
+        [TestMethod]
+        public async Task SaveNotExistsConfigurationAsync()
+        {
+            var config = new ConfigTest()
+            {
+                Description = "Test",
+                Name = "Test"
+            };
+
+            _mockedStorageService.Setup(x => x.Exists(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(() => false);
+            _mockedStorageService.Setup(x => x.StoreAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(() => Task.FromResult("NoPath"));
+
+            var manager = new ConfigurationManager();
+            var result = await manager.SaveAsync(config);
+
+            Assert.AreEqual(result, true);
+
+            _mockedStorageService.Verify(x => x.Exists(It.IsAny<string>(), It.IsAny<string>()));
+            _mockedStorageService.Verify(x => x.DeleteFile(It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+            _mockedStorageService.Verify(x => x.StoreAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+        }
+
+        [TestMethod]
+        public async Task SaveExistsConfigurationAsync()
+        {
+            var config = new ConfigTest()
+            {
+                Description = "Test",
+                Name = "Test"
+            };
+
+            _mockedStorageService.Setup(x => x.Exists(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(() => true);
+            _mockedStorageService.Setup(x => x.DeleteFile(It.IsAny<string>(), It.IsAny<string>()));
+            _mockedStorageService.Setup(x => x.StoreAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(() => Task.FromResult("NoPath"));
+
+            var manager = new ConfigurationManager();
+            var result = await manager.SaveAsync(config);
+
+            Assert.AreEqual(result, true);
+
+            _mockedStorageService.Verify(x => x.Exists(It.IsAny<string>(), It.IsAny<string>()));
+            _mockedStorageService.Verify(x => x.DeleteFile(It.IsAny<string>(), It.IsAny<string>()));
+            _mockedStorageService.Verify(x => x.StoreAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+        }
+    }
+}
---
+++ Tests/Excalibur.Shared.Tests/ObjectMapper/ObjectMapperTests.cs
@@ -0,0 +1,120 @@
+﻿using Excalibur.Shared.ObjectConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Excalibur.Shared.Tests.ObjectMapper
+{
+    [TestClass]
+    public class ObjectMapperTests
+    {
+        #region InnerClasses
+
+        private class ObjectA
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+        private class ObjectB
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
+        private class ObjectObjectMapper : BaseObjectMapper<ObjectA, ObjectB>
+        {
+            public override ObjectB Map(ObjectA source)
+            {
+                var destination = base.Map(source);
+
+                destination.Name = source.Name;
+                destination.Description = source.Description;
+
+                return destination;
+            }
+
+            public override void UpdateDestination(ObjectA source, ObjectB destination)
+            {
+                destination.Name = source.Name;
+                destination.Description = source.Description;
+            }
+
+            public override void UpdateSource(ObjectB destination, ObjectA source)
+            {
+                source.Name = destination.Name;
+                source.Description = destination.Description;
+            }
+        }
+
+        #endregion
+
+        [ClassInitialize]
+        public static void ClassSetup(TestContext a)
+        {
+            
+            ObjectMapperRegistrar.RegisterBinding<IObjectMapper<ObjectA, ObjectB>>(() => new ObjectObjectMapper());
+        }
+
+        [TestMethod]
+        public void TestObjectMap()
+        {
+            var objectA = new ObjectA()
+            {
+                Name = "Object",
+                Description = "Object description"
+            };
+
+            IObjectMapper<ObjectA, ObjectB> mapper = new ObjectObjectMapper();
+            var objectB = mapper.Map(objectA);
+
+            Assert.AreEqual(objectA.Name, objectB.Name);
+            Assert.AreEqual(objectA.Description, objectB.Description);
+        }
+
+        [TestMethod]
+        public void TestObjectUpdateDestination()
+        {
+            var objectA = new ObjectA()
+            {
+                Name = "ObjectA",
+                Description = "ObjectA description"
+            };
+
+            var objectB = new ObjectB()
+            {
+                Name = "ObjectB",
+                Description = "ObjectB description"
+            };
+
+            IObjectMapper<ObjectA, ObjectB> mapper = new ObjectObjectMapper();
+            mapper.UpdateDestination(objectA, objectB);
+
+            Assert.AreEqual(objectA.Name, objectB.Name);
+            Assert.AreEqual(objectA.Name, "ObjectA");
+            Assert.AreEqual(objectA.Description, objectB.Description);
+            Assert.AreEqual(objectA.Description, "ObjectA description");
+        }
+
+        [TestMethod]
+        public void TestObjectUpdateSource()
+        {
+            var objectA = new ObjectA()
+            {
+                Name = "ObjectA",
+                Description = "ObjectA description"
+            };
+
+            var objectB = new ObjectB()
+            {
+                Name = "ObjectB",
+                Description = "ObjectB description"
+            };
+
+            IObjectMapper<ObjectA, ObjectB> mapper = new ObjectObjectMapper();
+            mapper.UpdateSource(objectB, objectA);
+
+            Assert.AreEqual(objectA.Name, objectB.Name);
+            Assert.AreEqual(objectA.Name, "ObjectB");
+            Assert.AreEqual(objectA.Description, objectB.Description);
+            Assert.AreEqual(objectA.Description, "ObjectB description");
+        }
+    }
+}
