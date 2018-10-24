23fc2d0b934d56f6204c6d3bce35b2d26e22444b
Xciles <xciles@gmail.com>
2018-10-24 20:37:07 +0200

    Removing empty files Moving to its own class

---
+++ Providers/Excalibur.Providers.EncryptedFileStorage/EncryptedFileStorageConfig.cs
@@ -0,0 +1,10 @@
+﻿using Excalibur.Providers.FileStorage;
+
+namespace Excalibur.Providers.EncryptedFileStorage
+{
+    public class EncryptedFileStorageConfig : FileStorageConfig, IEncryptedProviderConfig
+    {
+        public string ProtectedStoreKeyIdentifier { get; set; } = "ex.ps.key";
+        public string ProtectedStoreSaltIdentifier { get; set; } = "ex.ps.salt";
+    }
+}
---
+++ Providers/Excalibur.Providers.EncryptedFileStorage/IEncryptedProviderConfig.cs
@@ -0,0 +1,10 @@
+﻿using Excalibur.Base.Providers;
+
+namespace Excalibur.Providers.EncryptedFileStorage
+{
+    public interface IEncryptedProviderConfig : IProviderConfig
+    {
+        string ProtectedStoreKeyIdentifier { get; set; }
+        string ProtectedStoreSaltIdentifier { get; set; }
+    }
+}
