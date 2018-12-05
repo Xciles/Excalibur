0a8e7825fe161cc3bf3d129df28e1b758530dd86
Xciles <xciles@gmail.com>
2018-12-05 18:21:52 +0100

    Adding NotifyTask Adding Async MvvmCross value converter

---
+++ Excalibur.Avalon/Utils/PropertyChangedEventArgsCache.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Excalibur.Avalon.Utils
+{
+    /// <summary>
+    /// https://github.com/StephenCleary/Mvvm.Core/blob/master/src/Nito.Mvvm.Core/PropertyChangedEventArgsCache.cs
+    /// The MIT License (MIT)
+    ///
+    /// Copyright(c) 2015 Stephen Cleary
+    /// 
+    /// Provides a cache for <see cref="PropertyChangedEventArgs"/> instances.
+    /// </summary>
+    public sealed class PropertyChangedEventArgsCache
+    {
+        /// <summary>
+        /// The underlying dictionary. This instance is its own mutex.
+        /// </summary>
+        private readonly Dictionary<string, PropertyChangedEventArgs> _cache = new Dictionary<string, PropertyChangedEventArgs>();
+
+        /// <summary>
+        /// Private constructor to prevent other instances.
+        /// </summary>
+        private PropertyChangedEventArgsCache()
+        {
+        }
+
+        /// <summary>
+        /// The global instance of the cache.
+        /// </summary>
+        public static PropertyChangedEventArgsCache Instance { get; } = new PropertyChangedEventArgsCache();
+
+        /// <summary>
+        /// Retrieves a <see cref="PropertyChangedEventArgs"/> instance for the specified property, creating it and adding it to the cache if necessary.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that changed.</param>
+        public PropertyChangedEventArgs Get(string propertyName)
+        {
+            lock (_cache)
+            {
+                if (_cache.TryGetValue(propertyName, out var result))
+                {
+                    return result;
+                }
+
+                result = new PropertyChangedEventArgs(propertyName);
+                _cache.Add(propertyName, result);
+                return result;
+            }
+        }
+    }
+}
---
+++ Excalibur.Cross/Converters/AsyncMvxValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Excalibur.Avalon.Utils;
+using MvvmCross.Converters;
+
+namespace Excalibur.Cross.Converters
+{
+    /// <inheritdoc />
+    public abstract class AsyncMvxValueConverter : IMvxValueConverter
+    {
+        /// <inheritdoc />
+        object IMvxValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return NotifyTask.Create(Convert(value, targetType, parameter, culture));
+        }
+
+        /// <inheritdoc />
+        object IMvxValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return NotifyTask.Create(ConvertBack(value, targetType, parameter, culture));
+        }
+
+        public abstract Task<object> Convert(object value, Type targetType, object parameter, CultureInfo culture);
+        public abstract Task<object> ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+    }
+}
